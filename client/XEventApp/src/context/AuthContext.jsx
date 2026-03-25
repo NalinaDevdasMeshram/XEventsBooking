f04a48830839f91d752d8c2fb84bd7be7cfcff2a
nalina <naleenameshram85@gmail.com>
2026-03-25 17:47:09 +0530

    working

--- client/XEventApp/src/context/AuthContext.jsx
+++ client/XEventApp/src/context/AuthContext.jsx
@@ -1,5 +1,5 @@
 import { createContext, useState, useContext, useEffect } from "react";
-import { authAPI } from "../services/api";
+import { authAPI } from "./context/AuthContext";
 import toast from "react-hot-toast";
 import React from "react";
 const AuthContext = createContext();
